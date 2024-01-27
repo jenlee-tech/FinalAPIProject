5e0ad413129047b9daea2efd23ff2d44fb089283
Jennifer Lee <jenlee.tech@gmail.com>
2024-01-27 12:59:25 -0500

    able to launch and view endpoint menu-items

--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import MenuItem
+from rest_framework import viewsets
+from .serializers import MenuItemSerializer
 
 # Create your views here.
+
+
+class MenuItemsViewSet(viewsets.ModelViewSet):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    ordering_fields = ['price', 'inventory']
+    search_fields = ['title', 'category__title']
